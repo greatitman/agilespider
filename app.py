2b8d67eb52abfdcfa67cbe925ff32a96ce3eb9e5
greatitman <agiletiger@qq.com>
2016-04-17 09:25:42 +0800

    add the main class

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+import re
+#coding : utf-8
+
+
+class AgileSpider(object):
+    def __init__(self):
+        print "init the spider "
+
+    @classmethod
+    def testStub(cls):
+        print "hello aigle spider "
+
+if __name__=='__main__':
+    AgileSpider.testStub()
